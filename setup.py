bc50d8e49291f7a82b1c8d15a2cd8c371665b6e1
Wes Okes <wes.okes@gmail.com>
2017-12-08 14:52:34 -0500

    django 2

--- setup.py
+++ setup.py
@@ -30,21 +30,18 @@ setup(
     packages=find_packages(),
     classifiers=[
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Framework :: Django',
-        'Framework :: Django :: 1.9',
-        'Framework :: Django :: 1.10',
         'Framework :: Django :: 1.11',
+        'Framework :: Django :: 2.0',
     ],
     license='MIT',
     install_requires=[
-        'django>=1.8',
+        'Django>=1.11',
     ],
     tests_require=[
         'psycopg2',
