fcea8734cc46aef2c7c17b504996da9ab8e2bd2a
Wes Okes <wes.okes@gmail.com>
2019-07-11 14:30:18 -0400

    requirements

--- setup.py
+++ setup.py
@@ -36,14 +36,13 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Framework :: Django',
-        'Framework :: Django :: 1.11',
         'Framework :: Django :: 2.0',
         'Framework :: Django :: 2.1',
         'Framework :: Django :: 2.2',
     ],
     license='MIT',
     install_requires=[
-        'Django>=1.11',
+        'Django>=2.0',
     ],
     tests_require=[
         'psycopg2',
